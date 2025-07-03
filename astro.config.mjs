e3da8cac0111f9ea3fa89d609a31ea1f9cedbbab
Christian Maaß <christianmaass@MacBook-Air-von-Christian-2.local>
2025-07-03 16:11:39 +0200

    Aktualisiertes Design und Bugfixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/christian-maass/',
+});
+// This configuration sets the base path for the Astro project to '/christian-maass/'.
